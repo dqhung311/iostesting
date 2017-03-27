62d0ea8b8e349ff51a8b97a6078daa0b248bcc18
Dao Quang Hung <daoquanghung@Daos-MacBook-Pro.local>
2017-03-27 20:45:32 +0700

    + commit file

--- MyApp/MyApp/ViewController.swift
+++ MyApp/MyApp/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var name: String
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
